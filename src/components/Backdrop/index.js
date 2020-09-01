18d4ce2ce4f2e508b545e7844ccdc1c11cad3557
hryhorii-kharchenko <shadowofnothing3@gmail.com>
2020-09-01 12:42:16 +0300

    iphone fixes v4

--- src/components/Backdrop/index.js
+++ src/components/Backdrop/index.js
@@ -3,7 +3,7 @@ import './style.module.scss';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-function Backdrop({ onClick, isActive, zIndex, backgroundColor, opacity }) {
+function Backdrop({ onClick, isActive, zIndex, backgroundColor }) {
   const styleName = 'backdrop ' + (isActive ? 'active' : '');
 
   return (
@@ -11,8 +11,7 @@ function Backdrop({ onClick, isActive, zIndex, backgroundColor, opacity }) {
       onClick={onClick}
       style={{
         backgroundColor,
-        // opacity: isActive ? opacity : '0%',
-        zIndex: isActive ? zIndex : '-999',
+        zIndex: zIndex,
       }}
       styleName={styleName}
     />
@@ -23,7 +22,6 @@ Backdrop.defaultProps = {
   isActive: false,
   zIndex: '999',
   backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  opacity: '50%',
 };
 
 Backdrop.propTypes = {
@@ -31,7 +29,6 @@ Backdrop.propTypes = {
   isActive: PropTypes.bool,
   zIndex: PropTypes.string,
   backgroundColor: PropTypes.string,
-  opacity: PropTypes.string,
 };
 
 export default Backdrop;
